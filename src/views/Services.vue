98ba22922be6c76ba2059baf88bbd3254a5f8187
JulitoM3 <l161080043@iztapalapa.tecnm.mx>
2021-06-04 19:34:34 -0500

    componente de Home sin vista

---
+++ src/views/Services.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-row>
+      <ServicesComponent
+        v-for="section in sections"
+        :key="section.id"
+        :section="section"
+      />
+    </b-row>
+  </div>
+</template>
+
+<script>
+import ServicesComponent from '@/components/ServicesComponent.vue'
+import SectionService from '@/API-Services/SectionsService.js'
+export default {
+  name: 'Services',
+  components: {
+    ServicesComponent,
+  },
+  data() {
+    return {
+      sections: [],
+    }
+  },
+  created() {
+    SectionService.getSections()
+      .then((response) => {
+        this.sections = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
